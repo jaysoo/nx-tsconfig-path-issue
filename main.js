ba935d141948561a304215e327068d745ff52fcf
Jack Hsu <jack.hsu@gmail.com>
2023-10-23 13:28:33 -0400

    add foo

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const { join } = require('node:path');
 const { register } = require('tsconfig-paths');
 const config = join(__dirname, 'tsconfig.json');
-register({
-  baseUrl: '.',
-  paths: require(config).compilerOptions.paths,
-})
-// require('ts-node').register(config);
-// require('@nx/js/src/internal').registerTsProject(config)
+// If you comment this out, then paths will resolve correctly
+register({ baseUrl: '.', paths: require(config).compilerOptions.paths })
 console.log('>>> nx path', require.resolve('nx'));
+// This will not resolve without the register above, since foo is not a valid package
+console.log('>>> foo path', require.resolve('foo'));
