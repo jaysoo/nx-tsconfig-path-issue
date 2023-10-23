c2fce4d762d1e0851756646d8aeaf74bdff85da2
Nicholas Cunningham <ndcunningham@gmail.com>
2023-10-23 12:20:59 -0600

    Add workaround with comments for require.resolve

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ register({ baseUrl: '.', paths: require(config).compilerOptions.paths })
 console.log('>>> nx path', require.resolve('nx'));
 // This will not resolve without the register above, since foo is not a valid package
 console.log('>>> foo path', require.resolve('foo'));
+
+// Fix/Workaround
+console.log('>>> nx path', require.resolve('nx/'));
+console.log('>>> foo path', require.resolve('foo/'));
+// These will work as it forces Node to look in a directory name nx/ or foo/ respectively in node_modules
+// The result will work as nx will be resolved but foo will not be resolved
